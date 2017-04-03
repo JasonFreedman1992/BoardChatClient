8e21a560534e222b0dc98b09536232b05ab1ab01
Jason <Jasonfreedman1992@gmail.com>
2017-04-03 15:50:20 -0700

    setting up exception handling, if servers are down, planning out login cache

--- ClientWindow.java
+++ ClientWindow.java
@@ -5,6 +5,7 @@ import java.net.Socket;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.ConnectException;
 
 public class ClientWindow extends JFrame
 {
@@ -13,15 +14,21 @@ public class ClientWindow extends JFrame
 	public JTextField sendText = new JTextField();
 	public JTextField chatText = new JTextField();
 
+	public JTextField username = new JTextField();
+	public JTextField password = new JTextField();
+
+	public JLabel offlineResponse = new JLabel();
+
     public void init(int p_width, int p_height) throws IOException
     {
-        setTitle("ChaBtchatroard");
+        setTitle("ChatBoard");
         setSize(p_width, p_height);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(null);
         setVisible(true);
-        initFields();
+        initConnection();
+        //initFields();
     }
 
     private void initFields() throws IOException
@@ -33,18 +40,7 @@ public class ClientWindow extends JFrame
     	{
     		public void actionPerformed(ActionEvent e)
     		{
-    			String serverAddress = JOptionPane.showInputDialog("Enter IP Address of a machine that is \n" + "running the date service on port 49152:");
-    			try
-    			{
-    				Socket s = new Socket(serverAddress, 49152);
-					BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-					String answer = input.readLine();
-					chatText.setText(answer);
-    			}
-    			catch(IOException f)
-    			{
 
-    			}
     		}
     	});
     	add(connectChat);
@@ -66,5 +62,40 @@ public class ClientWindow extends JFrame
     	chatText.setEditable(false);
     	add(chatText);
     	chatText.setVisible(true);
+    }
+
+    private void initSignIn()
+    {
+    	username.setSize(200, 25);
+    	username.setLocation(38, 350);
+    	add(username);
+    	username.setVisible(true);
+
+    	password.setSize(200, 25);
+    	password.setLocation(38, 390);
+    	add(password);
+    	password.setVisible(true);
+    }
+
+    private void initOffline()
+    {
+    	String sorry = "Sorry, We're Offline Right now\n we're working on Maintenance!";
+    }
+
+    private void initConnection() throws IOException
+    {
+    	try
+    	{
+    		String serverAddress = "54.70.172.148";
+    		Socket s = new Socket(serverAddress, 49152);
+			BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			String answer = input.readLine();
+			initSignIn();
+			username.setText(answer);
+		}
+		catch(ConnectException e)
+		{
+
+		}		
     }
 }
