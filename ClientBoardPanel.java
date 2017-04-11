4e7119fac5051fdd24cdec98bd5a5a0cee6b8aea
jason <Jasonfreedman1992@gmail.com>
2017-04-11 15:13:15 -0700

    changes

--- ClientBoardPanel.java
+++ ClientBoardPanel.java
@@ -2,6 +2,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JTextArea;
 import javax.swing.JButton;
+import javax.swing.JScrollPane;
 import net.miginfocom.swing.*;
 import java.awt.Color;
 import java.awt.event.*;
@@ -12,27 +13,57 @@ public class ClientBoardPanel extends JPanel
 {
 	JTextField chatSend = new JTextField(12);
 	JTextArea chatReceive = new JTextArea(10, 20);
+	JTextArea instanceInfo = new JTextArea(10, 20);
+	JScrollPane scrollpane = new JScrollPane(chatReceive);
+	JPanel drawPanel = new JPanel();
 	//JTextField chatReceive = new JTextField(17);
 	JButton send = new JButton("Send");
+	JButton clear = new JButton("Clear");
 	ClientBoardPanel()
 	{
+		drawPanel.setBackground(Color.white);
+		drawPanel.setMinimumSize(new Dimension(200,200));
 		chatReceive.setBackground(Color.white);
 		chatReceive.setLineWrap(true);
 		//chatReceive.setEditable(false);
 		//chatReceive.setMultipleMode(true);
 		setBackground(Color.lightGray);
-		setLayout(new MigLayout("alignx center, aligny center"));
+		setLayout(new MigLayout(""));
 		chatReceive.setPreferredSize(new Dimension(40, 40));
 		chatReceive.setMaximumSize(new Dimension(200, 100));
-		add(chatReceive, "center, wrap 5px");
-		add(chatSend, "left, dock south");
-		add(send,"right, dock south");
+		add(scrollpane, "center, wrap 5px");
+		add(chatSend, "center, wrap");
+		add(send, "center, wrap");
+		add(clear, "center");
+		add(drawPanel, "cell 1 0 1 2, grow");
+		add(instanceInfo, "cell 2 0 1 2, grow");
 		send.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent e)
 			{
-				chatReceive.setText(chatReceive.getText() + "\n" + chatSend.getText());
-				chatSend.setText("");
+				if(chatReceive.getText().equals(""))
+				{
+					chatReceive.setText(chatSend.getText()); 
+				}
+				else
+				{
+					chatReceive.setText(chatReceive.getText() + "\n" + chatSend.getText());
+					chatSend.setText("");
+				}
+			}
+		});
+		clear.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				if(chatReceive.getText().equals(""))
+				{
+
+				}
+				else
+				{
+					chatReceive.setText("");
+				}
 			}
 		});
 		MouseListen listen = new MouseListen();
