586df8b90614dc066c4d982f013fae6cf7e7e1c0
jason <Jasonfreedman1992@gmail.com>
2017-04-18 22:13:17 -0700

    changes

--- ClientBoardPanel.java
+++ ClientBoardPanel.java
@@ -97,7 +97,6 @@ public class ClientBoardPanel extends JPanel
 				{
 
 					System.out.println(clientData.receiveImg.remaining());
-					clientData.receiveImg.flip();
 					//System.out.println(clientData.receiveImg.position());
 					drawPanel.paintImage = drawPanel.createImageFromBytes();
 					drawPanel.repaint();
