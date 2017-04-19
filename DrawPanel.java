b6618025692b467de0c31d7275c426be63c86b6f
jason <Jasonfreedman1992@gmail.com>
2017-04-19 15:07:29 -0700

    changes

--- DrawPanel.java
+++ DrawPanel.java
@@ -19,6 +19,7 @@ public class DrawPanel extends JPanel
     // draw painting
     public void updatePaint()
     {
+    	paintImage = new BufferedImage(200, 200, BufferedImage.TYPE_3BYTE_BGR);
         Graphics g = paintImage.createGraphics();
         g.setColor(Color.yellow);
      	g.fillOval(clientData.mouseX-10, clientData.mouseY-10, 20, 20);
