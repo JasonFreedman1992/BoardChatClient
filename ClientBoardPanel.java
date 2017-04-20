69d5bdd34342896d003ae5b8d86f49959400785d
jason <Jasonfreedman1992@gmail.com>
2017-04-20 14:43:50 -0700

    test

--- ClientBoardPanel.java
+++ ClientBoardPanel.java
@@ -96,11 +96,11 @@ public class ClientBoardPanel extends JPanel
 
 					System.out.println(clientData.receiveImg.remaining());
 					//System.out.println(clientData.receiveImg.position());
-					//drawPanel.paintImage = drawPanel.createImageFromBytes();
-					try{drawPanel.createImageFromBytes();}catch(IOException f){}
+					drawPanel.paintImage = drawPanel.createImageFromBytes();
+					//try{drawPanel.createImageFromBytes();}catch(IOException f){}
 					try
 					{
-						//System.out.println(drawPanel.paintImage.toString());
+						System.out.println(drawPanel.paintImage.toString());
 						drawPanel.save();
 						drawPanel.load();
 						drawPanel.repaint();
